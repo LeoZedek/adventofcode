94c4254fc1a24bfe2da00681ce3e8e583fb25226
LeoZedek <leo.zedek@gmail.com>
2024-12-23 12:27:13 +0100

    2024 J14 part2

--- 2024/J14/script2.py
+++ 2024/J14/script2.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 ROWS = 103
 COLUMNS = 101
 
+AROUND = [(1, 1), (1, -1), (1, 0)]
+
 class Robot:
 	def __init__(self, p_row, p_col, v_row, v_col):
 		self.p_row = p_row
@@ -14,6 +16,14 @@ class Robot:
 	def move(self, seconds):
 		self.p_row = (self.p_row + seconds * self.v_row) % ROWS
 		self.p_col = (self.p_col + seconds * self.v_col) % COLUMNS
+
+def add_border(matrix, char, width):
+	for i in range(len(matrix)):
+		matrix[i] = char * width + matrix[i] + char * width
+
+	matrix.append(char * len(matrix[0]))
+	matrix.insert(0, char * len(matrix[0]))
+
 
 def create_map(robots):
 	robots_position = defaultdict(int)
@@ -27,9 +37,31 @@ def create_map(robots):
 		_map.append([])
 
 		for j in range(COLUMNS):
-			_map.append(str(robots_positions[(i, j)]) if robots_positions[(i, j)] != 0 else '.')
+			_map[i].append(str(robots_position[(i, j)]) if robots_position[(i, j)] != 0 else '.')
+
+		_map[i] = ''.join(_map[i])
 
 	return _map
+
+def is_most_robots_close(robots, _map, percentage):
+	temp_map = _map.copy()
+	add_border(temp_map, '.', 1)
+
+	robots_close = 0
+	for robot in robots:
+		close = False
+
+		for di in range(-1, 2):
+			for dj in range(-1, 2):
+				if di == 0 and dj == 0:
+					if int(temp_map[robot.p_row + 1][robot.p_col + 1]) > 1:
+						close = True
+				elif temp_map[robot.p_row + 1+ di][robot.p_col + 1 + dj] != '.':
+					close = True
+		robots_close += close
+
+	return robots_close / len(robots) >= percentage
+
 
 with open('input') as file:
 	data = file.read().splitlines()
@@ -37,18 +69,27 @@ with open('input') as file:
 robots = set()
 
 for line in data:
-	pos = re.search('p=\d+,\d+', line).group()[2:]
+	pos = re.search(r'p=\d+,\d+', line).group()[2:]
 	p_col, p_row = list(map(int, pos.split(',')))
-	velocity = re.search('v=-?\d+,-?\d+', line).group()[2:]
+	velocity = re.search(r'v=-?\d+,-?\d+', line).group()[2:]
 	v_col, v_row = list(map(int, velocity.split(',')))
 
 	robots.add(Robot(p_row, p_col, v_row, v_col))
 
 quadrants = defaultdict(int)
-
-for robot in robots:
-	robot.move(1)
-
-
 with open('output', 'w') as file:
-	first_map = create_map(robots)
+
+	for i in range(10000):
+
+		for robot in robots:
+			robot.move(1)
+
+		_map = create_map(robots)
+
+		if is_most_robots_close(robots, _map, 0.7):
+			print(i + 1)
+
+			for row in _map:
+				file.write(row)
+				file.write('\n')
+			file.write('\n')
