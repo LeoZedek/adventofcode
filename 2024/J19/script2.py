0d6bb200f31a36da0181c58fd9428492d6bc7064
LeoZedek <leo.zedek@gmail.com>
2024-12-24 15:23:36 +0100

    2024 J19

---
+++ 2024/J19/script2.py
@@ -0,0 +1,33 @@
+def number_arrangement(towel, memo):
+	if towel == '':
+		return 1
+
+	if towel in memo:
+		return memo[towel]
+
+	result = 0
+
+	for pattern in available_patterns:
+		if len(pattern) <= len(towel) and pattern == towel[:len(pattern)]:
+			result += number_arrangement(towel[len(pattern):], memo)
+
+	memo[towel] = result
+	return memo[towel]
+
+
+with open('input') as file:
+	available_patterns = file.readline().strip().split(', ')
+	file.readline()
+
+	towel = file.readline().strip()
+	towels = set()
+	while towel != '':
+		towels.add(towel)
+		towel = file.readline().strip()
+
+r = 0
+memo = {}
+for towel in towels:
+	r += number_arrangement(towel, memo)
+
+print(r)
